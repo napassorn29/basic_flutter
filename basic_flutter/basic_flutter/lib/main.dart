3c6797665c9464e24ee7c5326c311f9ea389692b
napassorn29 <119843578+napassorn29@users.noreply.github.com>
2023-07-02 21:20:56 +0700

    icon

--- basic_flutter/basic_flutter/lib/main.dart
+++ basic_flutter/basic_flutter/lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text("add"),
+        child: Icon(Icons.add),
       ),
     );
   }

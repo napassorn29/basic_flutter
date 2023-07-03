e35d994852b68b9883e140d66536d2b6a7a3a9e4
napassorn29 <119843578+napassorn29@users.noreply.github.com>
2023-07-03 15:40:52 +0700

    Container/BoxDecoration (box for data or text)

--- basic_flutter/basic_flutter/lib/main.dart
+++ basic_flutter/basic_flutter/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       title: "My App",
       home: MyHomePage(),
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primarySwatch: Colors.lightBlue,
       ),
     );
   }
@@ -35,73 +35,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int number = 0; // build state
-
-  // กลุ่มข้อมูล
-  List<foodMenu> menu = [
-    foodMenu("กุ้งเผา", "500", "assets/images/picture1.jpg"),
-    foodMenu("ปลาทอด", "80", "assets/images/picture2.jpg"),
-    foodMenu("ส้มตำ", "60", "assets/images/picture3.jpg"),
-    foodMenu("ผัดไท", "40", "assets/images/picture4.jpg")
-  ];
-
   // แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("เลือกเมนูอาหาร"),
+          title: Text("รายรับ รายจ่าย"),
         ),
-        body: ListView.builder(
-            itemCount: menu.length,
-            itemBuilder: (BuildContext context, int index) {
-              foodMenu food = menu[index];
-              return ListTile(
-                leading: Image.asset(food.img),
-                title: Text(
-                  food.name,
-                  style: TextStyle(fontSize: 20),
-                ),
-                subtitle: Text("ราคา " + food.price + " บาท"),
-                onTap: () {
-                  print("คุณเลือกเมนูอาหาร " + food.name);
-                },
-              );
-            }));
-  }
-
-  void addNumber() {
-    setState(() {
-      number++;
-    });
-  }
-
-  void minusNumber() {
-    setState(() {
-      number--;
-    });
-  }
-
-// เตรียมข้อมูล
-  List<Widget> getData(int count) {
-// กลุ่มข้อมูล text widget เก็บไว้ในตัวแปรชื่อ data
-    List<Widget> data = [];
-    for (var i = 0; i < count; i++) {
-      var menu = ListTile(
-        title: Text(
-          "เมนูที่ ${i + 1}",
-          style: TextStyle(fontSize: 20),
-        ),
-        subtitle: Text("หัวข้อย่อยที่ ${i + 1}"),
-      );
-      data.add(menu);
-    }
-    data.add(
-      Text(
-        "$number",
-        style: TextStyle(fontSize: 60),
-      ),
-    );
-    return data;
+        body: Column(
+          children: [
+            //กล่องที่ใช้จองพื้นที่ ใส่รูปใส่สีได้
+            Container(
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 212, 251, 167),
+                  borderRadius: BorderRadius.circular(10)),
+              height: 100,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 142, 179, 100),
+                  borderRadius: BorderRadius.circular(10)),
+              height: 100,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 106, 144, 64),
+                  borderRadius: BorderRadius.circular(10)),
+              height: 100,
+            )
+          ],
+        ));
   }
 }
